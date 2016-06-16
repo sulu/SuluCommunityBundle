8221eafa9f4ae407155e757d5b778444c0ebbd6b
Alexander Schranz <alexander.schranz@massiveart.com>
2016-06-16 16:45:52 +0200

    add community manager service compiler pass

---
+++ DependencyInjection/CompilerPass/CommunityManagerCompilerPass.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ * This file is part of Sulu.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\CommunityBundle\DependencyInjection\CompilerPass;
+
+use Sulu\Bundle\CommunityBundle\DependencyInjection\Configuration;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\DefinitionDecorator;
+
+/**
+ * Create foreach configured webspace a community manager.
+ */
+class CommunityManagerCompilerPass implements CompilerPassInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function process(ContainerBuilder $container)
+    {
+        $config = $container->getParameter('sulu_community.config');
+
+        foreach ($config[Configuration::WEBSPACES] as $webspaceKey => $webspaceConfig) {
+            // Set firewall by webspace key
+            if ($webspaceConfig[Configuration::FIREWALL] === null) {
+                $webspaceConfig[Configuration::FIREWALL] = $webspaceKey;
+            }
+
+            // Set role by webspace key
+            if ($webspaceConfig[Configuration::ROLE] === null) {
+                $webspaceConfig[Configuration::ROLE] = ucfirst($webspaceKey) . 'User';
+            }
+
+            $definition = new DefinitionDecorator('sulu_community.community_manager');
+            $definition->replaceArgument(0, $webspaceConfig);
+            $definition->replaceArgument(1, $webspaceKey);
+
+            $container->setDefinition(
+                sprintf('sulu_community.%s.community_manager', $webspaceKey),
+                $definition
+            );
+        }
+    }
+}
--- DependencyInjection/SuluCommunityExtension.php
+++ DependencyInjection/SuluCommunityExtension.php
@@ -11,13 +11,10 @@
 
 namespace Sulu\Bundle\CommunityBundle\DependencyInjection;
 
-use Sulu\Bundle\CommunityBundle\Manager\CommunityManager;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
@@ -35,36 +32,7 @@ class SuluCommunityExtension extends Extension implements PrependExtensionInterf
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        foreach ($config[Configuration::WEBSPACES] as $webspaceKey => $webspaceConfig) {
-            // Set firewall by webspace key
-            if ($webspaceConfig[Configuration::FIREWALL] === null) {
-                $webspaceConfig[Configuration::FIREWALL] = $webspaceKey;
-            }
-
-            // Set role by webspace key
-            if ($webspaceConfig[Configuration::ROLE] === null) {
-                $webspaceConfig[Configuration::ROLE] = ucfirst($webspaceKey) . 'User';
-            }
-
-            $container->setDefinition(
-                sprintf('sulu_community.%s.community_manager', $webspaceKey),
-                new Definition(
-                    CommunityManager::class,
-                    [
-                        $webspaceConfig,
-                        $webspaceKey,
-                        new Reference('doctrine.orm.entity_manager'),
-                        new Reference('event_dispatcher'),
-                        new Reference('security.token_storage'),
-                        new Reference('sulu_security.token_generator'),
-                        new Reference('sulu_core.webspace.webspace_manager'),
-                        new Reference('sulu.repository.user'),
-                        new Reference('sulu.repository.role'),
-                        new Reference('sulu.repository.contact'),
-                    ]
-                )
-            );
-        }
+        $container->setParameter('sulu_community.config', $config);
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.xml');
