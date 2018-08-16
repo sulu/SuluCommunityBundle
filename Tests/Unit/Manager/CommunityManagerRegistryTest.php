d4e1585d07705ab9379440ecb60065b263f3ec8b
Johannes Wachter <johannes.wachter@sulu.io>
2018-08-16 09:50:57 +0200

    added registry for community-manager

---
+++ Tests/Unit/Manager/CommunityManagerRegistryTest.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of Sulu.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\CommunityBundle\Tests\Unit\Manager;
+
+use Sulu\Bundle\CommunityBundle\Manager\CommunityManager;
+use Sulu\Bundle\CommunityBundle\Manager\CommunityManagerRegistry;
+
+class CommunityManagerRegistryTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGet()
+    {
+        $manager = $this->prophesize(CommunityManager::class);
+        $registry = new CommunityManagerRegistry(['sulu_io' => $manager->reveal()]);
+
+        $this->assertEquals($manager->reveal(), $registry->get('sulu_io'));
+    }
+
+    public function testGetNotExists()
+    {
+        $this->setExpectedException(\Exception::class);
+
+        $registry = new CommunityManagerRegistry();
+
+        $registry->get('sulu_io');
+    }
+
+    public function testHas()
+    {
+        $manager = $this->prophesize(CommunityManager::class);
+        $registry = new CommunityManagerRegistry(['sulu_io' => $manager->reveal()]);
+
+        $this->assertTrue($registry->has('sulu_io'));
+        $this->assertFalse($registry->has('test_io'));
+    }
+}
