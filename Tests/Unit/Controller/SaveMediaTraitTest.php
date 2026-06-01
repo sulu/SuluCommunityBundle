329b7090efe2010e6f72ef7990f49a936567d6dc
Alexander Schranz <alexander@sulu.io>
2026-06-01 11:21:55 +0200

    Apply suggestions from code review
    Co-authored-by: Alexander Schranz <alexander@sulu.io>

--- Tests/Unit/Controller/SaveMediaTraitTest.php
+++ Tests/Unit/Controller/SaveMediaTraitTest.php
@@ -109,7 +109,6 @@ class SaveMediaTraitTest extends TestCase
         foreach ($this->tempFilePaths as $tempFilePath) {
             \unlink($tempFilePath);
         }
-        parent::tearDown();
     }
 
     public function testNoAvatarAndNoMedias(): void
